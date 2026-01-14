91f7bf1845288af1aaa512ccd865be7e686dfb8b
Lakhani Mohammad Raza <razalakhani21@gmail.com>
2026-01-14 22:36:18 +0530

    learn about orbit controls and it's properties

--- vite_learning_three_JS/src/main.js
+++ vite_learning_three_JS/src/main.js
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 
@@ -9,8 +10,12 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
+//! controls.update() must be called after any manual changes to the camera's transform
+
+camera.position
+
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe:true });
 const cube = new THREE.Mesh(geometry, material);
 
 scene.add(cube);
@@ -28,11 +33,20 @@ window.addEventListener("resize", () => {
 
 document.body.appendChild(renderer.domElement);
 
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.enableDamping=true;
+//* controls.autoRotate = true;
+//* controls.autoRotateSpeed = 12.0;
+controls.enableZoom = true;
+controls.dampingFactor = 0.001
+
+
 function animate() {
   window.requestAnimationFrame(animate);
   renderer.render(scene, camera);
-  cube.rotation.x -= 0.001;
-  cube.rotation.y += 0.01;
+  // cube.rotation.x -= 0.001;
+  // cube.rotation.y += 0.01;
+  controls.update();
 }
 animate();
 
