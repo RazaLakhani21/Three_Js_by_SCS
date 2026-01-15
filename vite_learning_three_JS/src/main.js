255f3ef6a878c5ba1dc777e0d39c9fa2b8c9e184
Lakhani Mohammad Raza <razalakhani21@gmail.com>
2026-01-15 18:02:04 +0530

    learn about lil-gui and make a Controls, Material and Mesh Panel for Editing

--- vite_learning_three_JS/src/main.js
+++ vite_learning_three_JS/src/main.js
@@ -1,37 +1,43 @@
 import * as THREE from "three";
+import * as lil from "lil-gui";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
 const scene = new THREE.Scene();
 
-
 const highIntensityLight = new THREE.DirectionalLight(0xffffff, 2); // white light with intensity 1 (simulates sunlight)
-highIntensityLight.position.set(10, 20, 15)
+highIntensityLight.position.set(10, 20, 15);
 scene.add(highIntensityLight);
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5); // white light with intensity 0.5 (provides additional lighting)
-directionalLight.position.set(5, 10, 7.5)
+directionalLight.position.set(5, 10, 7.5);
 scene.add(directionalLight);
 
 const ambientLight = new THREE.AmbientLight(0x404040); // soft white light (fills shadows with a soft light)
 scene.add(ambientLight);
 
 const pointLight = new THREE.PointLight(0xffffff, 1, 100); // white light with intensity 1 and distance 100 (simulates a light bulb)
-pointLight.position.set(0, 5, 0)
+pointLight.position.set(0, 5, 0);
 scene.add(pointLight);
 
-const highIntensityLightHelper = new THREE.DirectionalLightHelper(highIntensityLight, 5)
-scene.add(highIntensityLightHelper)
-const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5)
-scene.add(directionalLightHelper)
+const highIntensityLightHelper = new THREE.DirectionalLightHelper(
+  highIntensityLight,
+  5
+);
+scene.add(highIntensityLightHelper);
+const directionalLightHelper = new THREE.DirectionalLightHelper(
+  directionalLight,
+  5
+);
+scene.add(directionalLightHelper);
 // const ambientLightHelper = new THREE.AmbientLightHelper()
-const pointLightHelper = new THREE.PointLightHelper(pointLight, 5)
-scene.add(pointLightHelper)
+const pointLightHelper = new THREE.PointLightHelper(pointLight, 5);
+scene.add(pointLightHelper);
 
-let loader = new THREE.TextureLoader()
-let color = loader.load("../text/color.jpg")
-let roughness = loader.load("../text/roughness.jpg")
-let normal = loader.load("../text/normal.png")
-let height = loader.load("../text/height.png")
+let loader = new THREE.TextureLoader();
+let color = loader.load("../text/color.jpg");
+let roughness = loader.load("../text/roughness.jpg");
+let normal = loader.load("../text/normal.png");
+let height = loader.load("../text/height.png");
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -42,7 +48,7 @@ const camera = new THREE.PerspectiveCamera(
 
 //! controls.update() must be called after any manual changes to the camera's transform
 
-camera.position
+camera.position;
 
 const geometry = new THREE.BoxGeometry(3, 1.8, 2);
 // const geometry = new THREE.SphereGeometry(1, 5, 5, 2.5, 1.2);
@@ -52,14 +58,24 @@ const geometry = new THREE.BoxGeometry(3, 1.8, 2);
 // const geometry = new THREE.CylinderGeometry(2, 2, 3, 10, 10, false, 150, 105);
 // const material = new THREE.MeshBasicMaterial({ color: "red", wireframe:false, roughness: 1, metalness: 1 });
 // const material = new THREE.MeshStandardMaterial({ color: "red", wireframe:false, roughness: 1, metalness: 1 });
-const material = new THREE.MeshStandardMaterial({ map: color, roughnessMap: roughness, normalMap: normal, displacementMap: height, displacementScale: 0.8 });
+const material = new THREE.MeshStandardMaterial({
+  map: color,
+  roughnessMap: roughness,
+  normalMap: normal,
+  // displacementMap: height,
+  // displacementScale: 0.8,
+});
 const cube = new THREE.Mesh(geometry, material);
 
 scene.add(cube);
 camera.position.z = 5;
 
 const canvas = document.querySelector(".draw");
-const renderer = new THREE.WebGLRenderer({ canvas, antialias: true, side: THREE.DoubleSide });
+const renderer = new THREE.WebGLRenderer({
+  canvas,
+  antialias: true,
+  side: THREE.DoubleSide,
+});
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 //! responsiveness
@@ -69,16 +85,36 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
 });
 
+//Create a GUI panel for material and mesh settings
+const gui = new lil.GUI();
+
+// Material Settings
+const materialFolder = gui.addFolder("Material");
+materialFolder.add(material, "roughness", 0, 1).name("Roughness");
+materialFolder.add(material, "metalness", 0, 1).name("Metalness");
+materialFolder.addColor(material, "color", 0, 1).name("Color");
+materialFolder.open();
+
+//Mesh settings
+const meshFolder = gui.addFolder("Mesh")
+meshFolder.add(cube.scale, 'x', 0.1, 5).name('Scale X')
+meshFolder.add(cube.scale, 'y', 0.1, 5).name('Scale Y')
+meshFolder.add(cube.scale, 'z', 0.1, 5).name('Scale Z')
+meshFolder.add(cube.position, 'x', -10, 10).name('Position X')
+meshFolder.add(cube.position, 'y', -10, 10).name('Position Y')
+meshFolder.add(cube.position, 'z', -10, 10).name('Position Z')
+meshFolder.open();
+
+
 document.body.appendChild(renderer.domElement);
 
 //! controlling orbits(Orbit Control)
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping=true;
+controls.enableDamping = true;
 // controls.autoRotate = true;
 //* controls.autoRotateSpeed = 12.0;
 controls.enableZoom = true;
-controls.dampingFactor = 0.021
-
+controls.dampingFactor = 0.021;
 
 function animate() {
   window.requestAnimationFrame(animate);
